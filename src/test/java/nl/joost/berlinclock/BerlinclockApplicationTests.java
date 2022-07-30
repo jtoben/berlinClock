85857442a30428d0de6c4be7549a9174ce48711a
Joost Toben <joost.toben@randstadgroep.nl>
2022-07-30 12:10:57 +0200

    setup project

---
+++ src/test/java/nl/joost/berlinclock/BerlinclockApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.joost.berlinclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
